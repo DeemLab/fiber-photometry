27592081365ec936f82d21bee2f47814d90179f7
padma <padmag@uw.edu>
2021-07-17 02:46:38 -0700

    fix time trim

--- python/input_to_extract.py
+++ python/input_to_extract.py
@@ -2,7 +2,7 @@ from tdt import read_block
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import math
+import bisect as b
 import statistics
 
 
@@ -41,9 +41,10 @@ def process(idx, GCAMP, ISOS, num, filename1, filename2, data, trimstart, trimen
 
 		# trim
 		fs = data.streams[GCAMP.title()].fs
-		start = int(math.floor(trimstart * fs))
-		stop = int(math.ceil(trimend * fs))
+		start = trimstart
+		stop = trimend
 		time = np.arange(start=1, stop=len(data.streams[GCAMP.title()].data)+1, step=1)/data.streams[GCAMP.title()].fs
+
 		data_GCAMP = np.array(data.streams[GCAMP.title()].data)
 		data_ISOS = np.array(data.streams[ISOS.title()].data)
 		Signal470 = data_GCAMP[start:-stop]
@@ -52,13 +53,12 @@ def process(idx, GCAMP, ISOS, num, filename1, filename2, data, trimstart, trimen
 
 		# downsample
 		N = fs
-		arr_GCAMP = (np.arange(start = 1, stop = len(Signal470)-N+2, step = N)).astype(int)
+		arr_GCAMP = (np.arange(start = 1, stop = len(Signal470) - N + 1, step = N)).astype(int)
 		down_GCAMP = [statistics.mean(Signal470[i:round(i+N-1)]) for i in arr_GCAMP]
-		arr_ISOS = (np.arange(start=1, stop=len(Signal405) - N + 2, step=N)).astype(int)
+		arr_ISOS = (np.arange(start=1, stop=len(Signal405) - N + 1, step=N)).astype(int)
 		down_ISOS = [statistics.mean(Signal405[i:round(i + N - 1)]) for i in arr_ISOS]
-
-		downtime = trimtime[0::round(N)]
-		downt = downtime[0:len(down_GCAMP)]
+		downtime = trimtime[::round(N)]
+		downt = downtime[:len(down_GCAMP)]
 
 		# smooth
 		smooth_win = 10
@@ -72,30 +72,30 @@ def process(idx, GCAMP, ISOS, num, filename1, filename2, data, trimstart, trimen
 		dFF = 100*dF/fit_ISOS
 
 		# plot
-		fig = plt.figure(figsize=(20, 12))
+		fig = plt.figure(figsize=(7, 10))
 		ax = fig.add_subplot(411)
 		p1, = ax.plot(time, data.streams[GCAMP.title()].data, linewidth=2,
 					   color='green', label='GCaMP')
 		p2, = ax.plot(time, data.streams[ISOS.title()].data, linewidth=2,
-					  color='dodgerblue', label='Isosbestic')
+					  color='blueviolet', label='Isosbestic')
 		ax.set_xlabel('Time (s)')
 		ax.set_ylabel('mV')
 		ax.set_title('Raw Signals ' + str(file))
-		ax.legend(handles=[p1, p2], loc='upper right')
-		ax.set_xlim([time[0], time[len(time)-1]])
+		ax.legend(handles=[p1, p2], loc='lower left')
+		ax.set_xlim([30, time[len(time)-1]])
 
 		ax2 = fig.add_subplot(412)
 		p3, = ax2.plot(downt, smooth_GCAMP, linewidth=2,
 					  color='green', label='Processed GCaMP')
 		p4, = ax2.plot(downt, smooth_ISOS, linewidth=2,
-					  color='dodgerblue', label='Processed Isosbestic')
+					  color='blueviolet', label='Processed Isosbestic')
 		p5, = ax2.plot(downt, fit_ISOS, linewidth=2,
-					   color='dodgerblue', label='Fit Isosbestic')
+					   color='blueviolet', label='Fit Isosbestic')
 		ax2.set_xlabel('Time (s)')
 		ax2.set_ylabel('mV')
-		ax2.set_title('Smoothed, Fit and Aligned Signals ' + str(file))
-		ax2.legend(handles=[p3, p4, p5], loc='upper right')
-		ax2.set_xlim([downt[0], len(downt)])
+		ax2.set_title('Smoothed, Fit, and Aligned Signals ' + str(file))
+		ax2.legend(handles=[p3, p4, p5], loc='lower left')
+		ax2.set_xlim([start, len(downt)-stop])
 
 		ax3 = fig.add_subplot(413)
 		p6, = ax3.plot(downt, dF, linewidth=2,
@@ -103,8 +103,8 @@ def process(idx, GCAMP, ISOS, num, filename1, filename2, data, trimstart, trimen
 		ax3.set_xlabel('Time (s)')
 		ax3.set_ylabel(r'$\Delta$mV')
 		ax3.set_title('Subtracted Signal ' + str(file))
-		ax3.legend(handles=[p6], loc='upper right')
-		ax3.set_xlim([downt[0], len(downt)])
+		ax3.legend(handles=[p6], loc='lower left')
+		ax3.set_xlim([start, len(downt)-stop])
 
 		ax4 = fig.add_subplot(414)
 		ax4.plot(downt, dFF, linewidth=2,
@@ -112,7 +112,7 @@ def process(idx, GCAMP, ISOS, num, filename1, filename2, data, trimstart, trimen
 		ax4.set_xlabel('Time (s)')
 		ax4.set_ylabel(r'$\Delta$F/F (%)')
 		ax4.set_title(r'$\Delta$F/F, ' + str(file))
-		ax4.set_xlim([downt[0], len(downt)])
+		ax4.set_xlim([start, len(downt)-stop])
 
 		fig.suptitle('Data Processing ' + str(file), fontsize=16)
 		fig.tight_layout()
@@ -126,7 +126,7 @@ def process(idx, GCAMP, ISOS, num, filename1, filename2, data, trimstart, trimen
 		ax.set_xlabel('Time (s)')
 		ax.set_ylabel(r'$\Delta$F/F (%)')
 		ax.set_title(r'$\Delta$F/F, ' + str(file))
-		ax.set_xlim([downt[0], len(downt)])
+		ax.set_xlim([start, len(downt)-stop])
 
 		fig2.tight_layout()
 		plt.savefig('dFF_' + str(file) + '.png')
@@ -138,8 +138,8 @@ def moving_average(x, w):
 
 def main():
 	tankdir = "/Volumes/GoogleDrive/Shared drives/Schwartz/Data/Fiber Photometry Experiments/Faber DMH Project/Circadian HFHS Presentation"
-	trimstart = 30
-	trimend = 30
+	trimstart = 20
+	trimend = 20
 	blockname = ['DMH-3-201108-123527', 'DMH-4-201108-132008', 'DMH-6-201108-140804', 'DMH-7-201108-153449',
 				  'DMH-3-201110-201611', 'DMH-4-201110-210026', 'DMH-6-201110-214402', 'DMH-7-201110-222751']
 	filename1 = ['ZT7-DMH3', 'ZT7-DMH4', 'ZT7-DMH6', 'ZT7-DMH7', 'ZT14-DMH3', 'ZT14-DMH4', 'ZT14-DMH6', 'ZT14-DMH7']
